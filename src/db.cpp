59e1aaafb11ba496dbdfad17308e85cf955a4ed6
Jillian Webb <jillbobaggins@pop-os.localdomain>
2024-04-28 22:26:36 -0600

    added db functions

--- src/db.cpp
+++ src/db.cpp
@@ -1,2 +1,78 @@
-#include "include/db.hpp"
+// ================================================================================
+// ================================================================================
+// - File:    db.cpp
+// - Purpose: This file manages a sqlite3 database.
+//
+// Source Metadata
+// - Author:  Jillian Webb
+// - Date:    April 28, 2024
+// - Version: 1.0
+// - Copyright: Copyright 2024, Jilly Webb Inc.
+// ================================================================================
+// ================================================================================
 
+#include "include/db.hpp"
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include "/usr/include/sqlite3.h"
+#include <exception>
+// ================================================================================
+// ================================================================================
+
+
+int createDB(const char* filename)
+{
+    sqlite3* db;
+    int exit = 0;
+
+    exit = sqlite3_open(filename, &db);
+    if (exit != SQLITE_OK){
+        std::cerr << "Error opening database: " << sqlite3_errmsg(db) << std::endl;
+        return exit;
+    }
+    std::cout << "Database: " << filename << " opened successfully" << std::endl;
+
+    sqlite3_close(db);
+
+    if (exit != SQLITE_OK) {
+        std::cerr << "Error closing database: " << sqlite3_errmsg(db) << std::endl;
+        return exit;
+    }
+
+    std::cout << "Database: " << filename << " closed successfully" << std::endl;
+
+    return 0;
+}
+// --------------------------------------------------------------------------------
+
+
+int createTable(const char* filename)
+{
+    sqlite3* db;
+
+    std::string sql = "CREATE TABLE IF NOT EXISTS PLANNER("
+    "ID INTEGER PRIMARY KEY AUTOINCREMENT, "
+    "TASK TEXT NOT NULL, "
+    "DUE_DATE VARCHAR(10) );";
+
+    try
+    {
+        int exit = 0;
+        exit = sqlite3_open(filename, &db);
+
+        char* messageError;
+        exit = sqlite3_exec(db, sql.c_str(), NULL, 0, &messageError);
+        if (exit != SQLITE_OK){
+            std::cerr << "Error with Create Table" << std::endl;
+            sqlite3_free(messageError);
+        }
+        else
+            std::cout << "Table created successfully" << std::endl;
+        sqlite3_close(db);
+    }
+    catch(const std::exception& e){
+        std::cerr << e.what();
+    }
+    return 0;
+}
---
+++ src/include/db.hpp
@@ -0,0 +1,8 @@
+#ifndef DB_H
+#define DB_H
+
+int createDB(const char* filename);
+
+int createTable(const char* filename);
+
+#endif
